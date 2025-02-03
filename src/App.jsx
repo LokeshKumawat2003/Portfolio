8179df2bbb4023023966ff92e8d5ce54e381c6db
lokesh <lokeshkumawat288g@gmail.com>
2025-02-03 14:17:09 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,5 @@
-// import About from "./Components/about/About"
-// import Footer from "./Components/Footer/Footer"
-// import Github from "./Components/github/Github"
-// import Home from "./Components/Home/Home"
-// import Navbar from "./Components/navbar/Navbar"
-// import Project from "./Components/project/Project"
-// import Skill from "./Components/skill/Skill"
 
-// function App() {
-
-
-//   return (
-//     <div style={{maxWidth:"1300px" ,margin:"auto"}}>
-
-//       <Navbar/>
-//       <Home/>
-//       <About/>
-//       <Skill/>
-//       <Project/>
-//       <Github/>
-//       <Footer/>
-//     </div>
-//   )
-// }
-
-// export default App
-import './app.css'
+import '../../Portfolio/src/Components/skill/homeapp.css'
 import Navbar from './Components/navbar/Navbar';
 import Github from './Components/github/Github';
 import Home from './Components/Home/Home';
